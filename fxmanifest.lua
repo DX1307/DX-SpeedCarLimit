d872df9c03b43d6c60c70abcc467237d6c02e541
DX HIGH <107764618+DX1307@users.noreply.github.com>
2023-11-03 00:52:36 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 author '! D X H I G H#3690'
 description 'DX-SpeedCarLimit Script'
-version '0.1'
+version '0.2'
+-- version '0.1'
 
 client_scripts {
 	'config.lua',
